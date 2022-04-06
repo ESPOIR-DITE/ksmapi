75fc4117852cd7bc3a55ef72e03bab82027595e0
Marilyn <espoirditekemena@gmail.com>
2022-04-06 11:01:04 +0200

    creating a delete metho for IngredientTransaction.

--- src/main/java/com/example/ksmapi/repository/sell/SellPeriodRepository.java
+++ src/main/java/com/example/ksmapi/repository/sell/SellPeriodRepository.java
@@ -4,10 +4,11 @@ import com.example.ksmapi.domain.sell.SellPeriod;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface SellPeriodRepository extends JpaRepository<SellPeriod,String> {
     List<SellPeriod> findAllByPeriodId(String periodId);
-    SellPeriod readByPeriodIdAndDateIsNear(String s);
+//    SellPeriod readByPeriodIdAndDate_Day(String s, Date date);
 }
--- src/main/java/com/example/ksmapi/service/sell/SellPeriodService.java
+++ src/main/java/com/example/ksmapi/service/sell/SellPeriodService.java
@@ -3,8 +3,10 @@ package com.example.ksmapi.service.sell;
 import com.example.ksmapi.domain.sell.SellPeriod;
 import com.example.ksmapi.service.ServiceInterface;
 
+import java.util.Date;
 import java.util.List;
 
 public interface SellPeriodService extends ServiceInterface<SellPeriod,String> {
     List<SellPeriod> findAllByPeriodId(String periodId);
+//    SellPeriod readByPeriodIdAnd(String s, Date date);
 }
